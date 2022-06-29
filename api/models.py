66a56ce1426ba3e69df95de38e3fa2568fee6221
medvedkokirill <medvedko.fortech@gmail.com>
2022-06-29 16:40:44 +0300

    add Authors model, AuthorModelSerializer, AuthorModelViewSet, urls

--- api/models.py
+++ api/models.py
@@ -1,3 +1,26 @@
+from uuid import uuid4
+
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4)
+    name = models.CharField(max_length=128)
+    picture = models.URLField()
+
+
+"""
+{
+  "id": "39df53da-542a-3518-9c19-3568e21644fe",
+  "author": {
+    "id": "2d460e48-a4fa-370b-a2d0-79f2f601988c",
+    "name": "Author Name",
+    "picture": "https://picture.url"
+  },
+  "category": "Category",
+  "title": "Article title",
+  "summary": "This is a summary of the article",
+  "firstParagraph": "<p>This is the first paragraph of this article</p>",
+  "body": "<div><p>Second paragraph</p><p>Third paragraph</p></div>"
+}
+"""
--- news/settings.py
+++ news/settings.py
@@ -162,10 +162,10 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
 
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ]
+    # 'DEFAULT_AUTHENTICATION_CLASSES': [
+    #     'rest_framework.authentication.BasicAuthentication',
+    #     'rest_framework.authentication.SessionAuthentication',
+    #     'rest_framework.authentication.TokenAuthentication',
+    #     'rest_framework_simplejwt.authentication.JWTAuthentication',
+    # ]
 }
